2b505a0fb1148d25c8a04c66f4854f374dd50a28
abouthernan <hernan@vehement.co>
2025-09-17 17:28:33 -0500

    Icon Languages

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -8,10 +8,12 @@ export const defaultLang = 'es';
 
 export const ui = {
   es: {
+    //navegación
     'nav.home': 'Inicio',
     'nav.about': 'Acerca',
     'nav.blog': 'Blog',
     'nav.contact': 'Contacto',
+    //Hero Index ES
     'hero.greeting': 'Hola 👋',
     'hero.name': 'Soy Hernán Estupiñán',
     'hero.role': 'Especialista en Growth Marketing y Producto',
@@ -19,10 +21,12 @@ export const ui = {
     'hero.resume': 'Ver CV'
   },
   en: {
+    //navegation
     'nav.home': 'Home',
     'nav.about': 'About',
     'nav.blog': 'Blog',
     'nav.contact': 'Contact',
+    //Hero Index EN
     'hero.greeting': 'Hey there 👋',
     'hero.name': "I'm Hernán Estupiñán",
     'hero.role': 'Growth Marketing and Product Specialist',
